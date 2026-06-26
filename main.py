410c6a348ed269c8b5f4a21bf5d9780975a6e8d6
Sujal Sharma <sharmasujalwins@outlook.com>
2026-06-26 15:47:54 +0530

    this is first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+app = FastAPI()
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(varchar(255), index=True)
+    description = Column(varchar(255), index=True)
+    quantity = Column(Integer, index=True)
+    price = Column(Float, index=True)
+    status = Column(String(50), index=True)
+        
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+ return {"item_id": item_id, "q": q}
+
