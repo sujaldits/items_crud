43248ef7b9f36692f68a108d85e315845f2840de
Sujal sharma <sharmasujalwins@outlook.com>
2026-06-29 10:37:35 +0530

    crud operation

--- main.py
+++ main.py
@@ -16,13 +16,94 @@ class Item(Base):
     price = Column(Float, index=True)
     status = Column(String(50), index=True)
 
-# this is comment
+#this is comment
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
+# ---------------- READ ALL ---------------- 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
  return {"item_id": item_id, "q": q}
 
+# ---------------- CREATE ----------------
+
+@app.post("/items")
+def create_item(
+    name: str,
+    description: str,
+    quantity: int,
+    price: float,
+    status: str,
+    db: Session = Depends(get_db)
+):
+
+    item = Item(
+        name=name,
+        description=description,
+        quantity=quantity,
+        price=price,
+        status=status
+    )
+
+    db.add(item)
+    db.commit()
+    db.refresh(item)
+
+    return {
+        "message": "Item Created Successfully",
+        "data": item
+    }
+
+
+# ---------------- UPDATE ----------------
+
+@app.put("/items/{item_id}")
+def update_item(
+    item_id: int,
+    name: str,
+    description: str,
+    quantity: int,
+    price: float,
+    status: str,
+    db: Session = Depends(get_db)
+):
+
+    item = db.query(Item).filter(Item.id == item_id).first()
+
+    if not item:
+        raise HTTPException(status_code=404, detail="Item Not Found")
+
+    item.name = name
+    item.description = description
+    item.quantity = quantity
+    item.price = price
+    item.status = status
+
+    db.commit()
+    db.refresh(item)
+
+    return {
+        "message": "Item Updated Successfully",
+        "data": item
+    }
+
+# ---------------- DELETE ----------------
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+
+    item = db.query(Item).filter(Item.id == item_id).first()
+
+    if not item:
+        raise HTTPException(status_code=404, detail="Item Not Found")
+
+    db.delete(item)
+    db.commit()
+
+    return {
+        "message": "Item Deleted Successfully"
+    }
